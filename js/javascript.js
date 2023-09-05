a3c4543d80bfd93f9b22494a35d84dc19f9ab075
EmersonFras <47959332+EmersonFras@users.noreply.github.com>
2023-09-04 20:38:44 -0400

    Add custom grid size and grid drawing
        Adds a prompt for users to choose the size of the grid. Along with the ability to change the grid boxes color by hovering over the grid boxes to draw.

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,33 @@
-// define the constant for the container
-const container = document.querySelector(".container");
+let gridLengths = prompt("How many squares do you want the sides to have?")
+createGrid(gridLengths);
 
-const gridLengths = 16;
+// define a list of all the grid boxes
+const boxes = document.querySelectorAll("#box");
 
-// iterate through grid lengths to create rows and columns of divs
-for (let j = 0; j < gridLengths; j++) {
-    // define a constant for the row div
-    const row = document.createElement("div");
-    row.setAttribute("class", "row");
-    // iterate through grid lengths to create the column divs
-    for (let i = 0; i < gridLengths; i++) {
-        const box = document.createElement("div");
-        box.setAttribute("class", "box");
-        row.appendChild(box);
+// add an event listener to each grid box for when the mouse is over it
+boxes.forEach(box => box.addEventListener("mouseover", gridDraw));
+
+
+function createGrid(gridLengths) {
+    // define the constant for the container
+    const container = document.querySelector(".container");
+
+    // iterate through grid lengths to create rows and columns of divs
+    for (let j = 0; j < gridLengths; j++) {
+        // define a constant for the row div
+        const row = document.createElement("div");
+        row.setAttribute("id", "row");
+        // iterate through grid lengths to create the column divs
+        for (let i = 0; i < gridLengths; i++) {
+            const box = document.createElement("div");
+            box.setAttribute("id", "box");
+            row.appendChild(box);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+}
+function gridDraw() {
+    // set attribute of this to hover to give the hover styling
+    this.setAttribute("class", "hover");
 }
 
